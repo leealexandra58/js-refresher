d344e09bad28dda4d2050afaa8a792b88b02f215
leeal <leealexandra58@gmail.com>
2022-07-07 15:26:09 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let firstName = "Allie";
+let lastName = "Lee";
+
+let fullName = (`${firstName} ${lastName}`);
+console.log(fullName);
+
+let population = 661193;
+console.log(population);
+
+console.log(population * 3);
+
+let javaScriptIsCool = true;
+let colors = ("red", "orange", "yellow", "green", "blue", "indigo", "violet");
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i <= 4; i++) {
+    console.log(i);
+}
